d9a5d4488493b84494f858e3ed6791c71953a6db
Ian Perez Bunuel <148461774+Ian-PB@users.noreply.github.com>
2025-10-09 22:56:39 +0100

    Fixed issue with walls

--- dingle-project/Scripts/WallShaderUpdater.cs
+++ dingle-project/Scripts/WallShaderUpdater.cs
@@ -9,6 +9,10 @@ public partial class WallShaderUpdater : Node3D
     [Export] public ShaderMaterial SharedWallMaterial;
     private bool playerSeen;
 
+    // Handle wall hit
+    MeshInstance3D hitObj;
+    MeshInstance3D prevHitObj;
+
     // Hole parameters
     private bool holeOn;
     private float holeSize = 1f;
@@ -25,7 +29,7 @@ public partial class WallShaderUpdater : Node3D
     {
         if (player != null && SharedWallMaterial != null)
         {
-            playerSeen = HasLineOfSight();
+            hitObj = HasLineOfSight(out playerSeen);
             SharedWallMaterial.SetShaderParameter("player_position", player.GlobalPosition);
 
             if (playerSeen)
@@ -45,22 +49,43 @@ public partial class WallShaderUpdater : Node3D
                 holeSize -= 0.001f;
             }
 
+            // Handle which is the newest hit
+            // turn off previous object
+            if (prevHitObj != null && prevHitObj != hitObj)
+            {
+                prevHitObj.SetInstanceShaderParameter("hole_visable", false);
+            }
 
-            SharedWallMaterial.SetShaderParameter("hole_visable", holeOn);
+            if (hitObj != null)
+            {
+                hitObj.SetInstanceShaderParameter("hole_visable", true);
+                prevHitObj = hitObj;
+            }
+            else if (prevHitObj != null)
+            {
+                prevHitObj.SetInstanceShaderParameter("hole_visable", false);
+                prevHitObj = null;
+            }
+
             SharedWallMaterial.SetShaderParameter("hole_size", holeSize);
         }
     }
 
-    public bool HasLineOfSight()
+    public MeshInstance3D HasLineOfSight(out bool t_playerSeen)
     {
         rayToPlayer.TargetPosition = rayToPlayer.ToLocal(player.GlobalPosition);
         rayToPlayer.ForceRaycastUpdate();
 
         if (rayToPlayer.GetCollider() == player)
         {
-            return true;
+            t_playerSeen = true;
+            return null;
         }
-
-        return false;
+        else
+        {
+            t_playerSeen = false;
+            return (rayToPlayer.GetCollider() as Node).GetParent() as MeshInstance3D;
+        }   
+            
     }
 }
