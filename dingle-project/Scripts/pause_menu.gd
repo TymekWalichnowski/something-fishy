407bcf7bceba9da2ccfc0aca10269f58407417a3
RionaKelly <c00296706@setu.ie>
2025-10-19 21:43:24 +0100

    pause menu added!

---
+++ dingle-project/Scripts/pause_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var main = $"../../"
+
+func _on_resume_pressed():
+	main.pauseMenu()
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
